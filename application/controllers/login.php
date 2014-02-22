93b190678235f85692ae2d2d6fda315928c7e702
carlokohan <carlohusmillo@yahoo.com>
2014-02-22 19:53:15 +0800

    Email after reserve is now functional

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -17,7 +17,9 @@ class Login extends CI_Controller{
 				'loggedIn' => TRUE,
 				'id' => $userData[0]->id,
 				'userType' => $userData[0]->user_type,
-				'username' => $userData[0]->username
+				'username' => $userData[0]->username,
+				'email_address' => $userData[0]->email_address,
+				'firstName' => $userData[0]->first_name
 				);
 
 			$this->session->set_userdata($sessionData);
